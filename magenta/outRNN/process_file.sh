48d1f8ae07359b9264cda6e807d5c826755f49cf
diego <diegovd0296@diegovd0296@gmail.com>
2018-05-30 02:24:10 +0200

    logs and audio crop

--- magenta/outRNN/process_file.sh
+++ magenta/outRNN/process_file.sh
@@ -13,7 +13,10 @@ IFS=''
 
 myfile2="${ADDR[0]}.mp3"
 myfile="${ADDR[0]}.mid"
+myfile3="${ADDR[0]}cut.mp3"
 
 timidity $mypath$myfile -Ow -o - | lame - -b 64 $mypath$myfile2
-lame --scale 30 $mypath$myfile2 $mypath$myfile2'a'
+ffmpeg -ss 00:00:05 -i $mypath$myfile2 $mypath$myfile3 -y
+lame --scale 30 $mypath$myfile3 $mypath$myfile2'a'
 scp $mypath$myfile2'a' pi@192.168.43.167:rlp/magenta_audio/
+
